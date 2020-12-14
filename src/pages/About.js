fca5e03cac11c7569deee7d1058f77ab8babb4fe
Yuanhong Shi <shiyuanhong@gmail.com>
2020-12-14 09:37:43 -0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, aperiam quis. Nam, eligendi? Reiciendis ad accusantium nostrum porro id possimus laboriosam delectus quasi at? Commodi quisquam iure repellat reiciendis repudiandae!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur accusantium laboriosam, aliquid reiciendis tempore eius quod laudantium fugiat atque, tenetur ipsa repellendus ad asperiores, delectus ea quos veniam. Omnis, nulla!
+      </p>
+    </section>
+  )
+}
+
+export default About
